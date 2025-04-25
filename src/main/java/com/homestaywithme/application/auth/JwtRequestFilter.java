6e823ab9db79d76984c26ad8314c3f9b2b748836
admin\admin <congtuyen2032001@gmail.com>
2025-04-25 23:28:26 +0700

    add spring security

---
+++ src/main/java/com/homestaywithme/application/auth/JwtRequestFilter.java
@@ -0,0 +1,58 @@
+package com.homestaywithme.application.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+    public static final String USER_ID_ATTRIBUTE = "userId";
+
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public JwtRequestFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
+
+        if(authorizationHeader == null ||
+                !authorizationHeader.startsWith(BEARER_PREFIX)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        String jwt = authorizationHeader.substring(BEARER_PREFIX.length());
+        var optionalClaims = jwtUtil.parse(jwt);
+        if(optionalClaims.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        var claims = optionalClaims.get();
+        var userId = Long.parseLong(claims.getBody().getSubject());
+        request.setAttribute(USER_ID_ATTRIBUTE, userId);
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                userId, null, null
+        );
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+}
